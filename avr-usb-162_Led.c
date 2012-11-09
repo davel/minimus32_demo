504d783bc5c506a519eb5d44482309e4520be44b
Dave Lambley <davel@davel-Aspire-One-522.(none)>
2012-11-09 09:15:07 +0000

    Simplify, pulling out the watchdog.

--- avr-usb-162_Led.c
+++ avr-usb-162_Led.c
@@ -14,29 +14,26 @@ void PORT_Init()
 	PORTC = 0b00000000;
 	DDRC = 0b00000000;
 
-	PORTD = 0b00000000;
+	PORTD = 0b10000000;
 	DDRD  = 0b01100000;		//set led as output
 }
 
 int i;
 
 int main(void)
-{	
+{
 	PORT_Init();
-/*	asm("WDR");						//Watchdog Timer Reset
+	asm("WDR");						//Watchdog Timer Reset
 	MCUSR= ~(1<<WDRF);
-	WDTCSR = (1<<WDCE) | (1<<WDE);
-	WDTCSR = 0x00;
-	WDTCKD = 0x00;
-*/
+	WDTCSR = 0x00;                  // Disable the watchdog
+	WDTCKD = 0x00;                  // No early warnings either
+
 	while (1)
 	{
         PORTD = 0b00100000;
         _delay_ms(100);
         PORTD = 0b01000000;
         _delay_ms(100);
-
-	    asm("WDR");						//Watchdog Timer Reset
     }
 	return 0;
 }
