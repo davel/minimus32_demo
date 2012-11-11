bc2cf4db59f2dd206397f64e659d9d5ae395fb83
Dave Lambley <davel@davel-Aspire-One-522.(none)>
2012-11-11 12:07:17 +0000

    Fix white space.

--- led.c
+++ led.c
@@ -4,25 +4,25 @@
 
 void PORT_Init()
 {
-	PORTB = 0b00000000;
-	DDRB = 0b00000000;
+    PORTB = 0b00000000;
+    DDRB = 0b00000000;
 
-	PORTC = 0b00000000;
-	DDRC = 0b00000000;
+    PORTC = 0b00000000;
+    DDRC = 0b00000000;
 
-	PORTD = 0b00000000;
-	DDRD  = 0b01100000;		//set LEDs as output
+    PORTD = 0b00000000;
+    DDRD  = 0b01100000;             //set LEDs as output
 }
 
 int main(void)
 {
-	PORT_Init();
-	asm("WDR");						//Watchdog Timer Reset
-	WDTCSR = 0x00;                  // Disable the watchdog
-	WDTCKD = 0x00;                  // No early warnings either
+    PORT_Init();
+    asm("WDR");                     //Watchdog Timer Reset
+    WDTCSR = 0x00;                  // Disable the watchdog
+    WDTCKD = 0x00;                  // No early warnings either
 
-	while (1)
-	{
+    while (1)
+    {
         if (PIND &  0b10000000) {
             PORTD = 0b00100000;
         }
@@ -30,6 +30,6 @@ int main(void)
             PORTD = 0b01000000;
         }
     }
-	return 0;
+    return 0;
 }
 
